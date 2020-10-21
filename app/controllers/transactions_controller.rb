e9bf365bd4ccf6ed09f49e73b987c805397f7efc
Ubuntu <ubuntu@ip-172-31-23-45.us-east-2.compute.internal>
2020-10-21 07:42:11 +0000

    Implement the transaction index method functionality to display all transactions of an account

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,14 @@
 class TransactionsController < ApplicationController
+  before_action :load_task
+  
   def index
+    @transactions = @account.transactions
   end
+  
+  private
+  
+  def load_task
+    @account = Account.find params[:account_id]
+  end
+  
 end
