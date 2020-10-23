1ac46b52733059a1e8d79cc1094a13ac6f97bd9a
Ubuntu <ubuntu@ip-172-31-23-45.us-east-2.compute.internal>
2020-10-23 22:46:43 +0000

    Implement the new and create actions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,14 +1,38 @@
 class TransactionsController < ApplicationController
-  before_action :load_task
+  before_action :load_account
+  before_action :load_transaction, except: [:index, :new, :create]
   
   def index
     @transactions = @account.transactions
   end
   
+  def new
+    @transaction = Transaction.new
+  end
+  
+  def create
+    @transaction = Transaction.new transaction_params
+    if @account.transactions << @transaction
+      redirect_to account_transactions_path(@account), notice: "Transaction Created."
+    else
+      render :new
+    end
+  end
+  
+  
+  
   private
   
-  def load_task
+  def load_account
     @account = Account.find params[:account_id]
+  end
+  
+  def load_transaction
+    @transaction = @account.transactions.find params[:id]
+  end
+  
+  def transaction_params
+    params.require(:transaction).permit(:transaction_type,:amount,:description)
   end
   
 end
