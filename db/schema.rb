d1e5b30c8e71c0a93e925053a103f04dec79e44e
KaipoJames <kaiposemail@yahoo.com>
2020-12-02 23:16:14 -1000

    install activeStorage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_085504) do
+ActiveRecord::Schema.define(version: 2020_12_03_091448) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "account_number"
@@ -19,6 +19,27 @@ ActiveRecord::Schema.define(version: 2020_12_03_085504) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "credit_cards", force: :cascade do |t|
@@ -53,4 +74,5 @@ ActiveRecord::Schema.define(version: 2020_12_03_085504) do
     t.string "token"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
