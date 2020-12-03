85cb9ee329f17a57328f77a35d0df02d6f23b4f0
KaipoJames <kaiposemail@yahoo.com>
2020-12-02 23:09:57 -1000

    Add all actions for credit_card controller

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,6 +1,6 @@
 class CreditCardsController < ApplicationController
   before_action :authenticate
-  #before_action :load_credit_card, except: [:index, :new, :create]
+  before_action :load_credit_card, except: [:index, :new, :create]
 
   def index
     @credit_cards = CreditCard.all
@@ -20,6 +20,29 @@ class CreditCardsController < ApplicationController
     end
   end
 
+  # GET, accounts/:id
+  def show
+  end
+  
+  # GET, accounts/:id/edit
+  def edit
+  end
+
+  # PATCH/PUT, accounts/:id
+  def update 
+    if @credit_card.update update_credit_card_params
+      redirect_to @credit_card, notice: "Card Updated"
+    else
+      render :edit
+    end
+  end
+  
+  # DELETE, accounts/:id
+  def destroy
+    @credit_card.destroy
+    redirect_to credit_cards_path, alert: "Card Deleted"
+  end
+
   private 
   def load_credit_card
     @credit_card = current_user.credit_cards.find params[:id]
@@ -29,7 +52,7 @@ class CreditCardsController < ApplicationController
     params.require(:credit_card).permit(:number, :security_code, :expiration_date)
   end
   
-  def update_account_params
+  def update_credit_card_params
     params.require(:credit_card).permit(:number)
   end
 
